e04f2c36f589f130718b038e16bcb499fd184f52
Bacchae <kciuk.barbara@gmail.com>
2020-04-25 14:35:37 +0000

    working winner

--- PPP.cs
+++ PPP.cs
@@ -1,8 +1,5 @@
 using System;
 
-/* pętla while
-tablica */
-
 public class PPP
 {
     public static void Main()
@@ -11,8 +8,11 @@ public class PPP
       string replay = null;
 
       do{
-        int P1 = Choice();
-        bool correct = Check(P1);
+        int P1 = Choice1();
+        int P2 = Choice2();
+
+        bool correct = Check(P1, P2);
+       // bool correct = Check(P2);
          //           System.Console.WriteLine("{0}", correct);
          //  System.Console.WriteLine("alalala, {0}", P1);
         if(correct == false){  
@@ -20,55 +20,62 @@ public class PPP
           Main();
         }else{
           System.Console.WriteLine("Player 1 chose {0}", P1);
+          System.Console.WriteLine("Player 2 chose {0}", P2);
+          Fight(P1, P2);
           System.Console.WriteLine("Do you want to play again? y/n");
           replay = System.Console.ReadLine();
         }
       }while(replay == "y");
     }
     
-/*
 
-      if (P1 == P2)  {
-          Console.WriteLine("It's a draft!");
-        } else if (P1 == "Rock" && P2 == "Scissors"
-                   || P1 == "Paper" && P2 == "Rock"
-                   || P1 == "Scissors" && P2 == "Paper") {
-            Console.WriteLine("{0} beats {1}! Player 1 wins!", P1, P2);
-        } else if (P1 == "Scissors" && P2 == "Rock"
-                   || P1 == "Rock" && P2 == "Paper"
-                   || P1 == "Paper" && P2 == "Scissors") {
-            Console.WriteLine("{0} beats {1}! Player 2 wins!", P2, P1);
-        }else{
-         Console.WriteLine("Oops! Something went wrong!");
-        }
-*/
-    
-
-    public static int Choice()  
+    public static int Choice1()  
     {
         System.Console.WriteLine("Player 1, choose from Sword [1], Bow [2], Axe [3]");
-        int weapon = int.Parse(Console.ReadLine());
-      //  Console.Clear();
-       // System.Console.WriteLine("Player 2, choose from Rock, Scissors, Paper");
-      //  string P2 = System.Console.ReadLine();
-        //Console.Clear();
-        //System.Console.WriteLine("Player 1 chose {0}", P1);
-        //System.Console.WriteLine("Player 2 chose {0}", P2);
-        
-
-        return weapon;
+        int weapon1 = int.Parse(Console.ReadLine());
+        return weapon1;
     }
 
-
-    public static bool Check(int P1)  
+    public static int Choice2()  
     {
-      if (P1 == 1 || P1 == 2 || P1 == 3) {
+      Random random = new Random();
+      int weapon2 = random.Next(1, 3);  
+        return weapon2;
+    }
+
+    public static bool Check(int P1, int P2)  
+    {
+      if ((P1 == 1 || P1 == 2 || P1 == 3) 
+         && (P2 == 1 || P2 == 2 || P2 == 3)) {
           return true;
       } else {
-      return false;               
+          return false;               
       }
     }
 
+    
+    public static int Fight(int P1, int P2)  
+    {
+      int winner = 1;
+      if (P1 == P2)  {
+          Console.WriteLine("It's a draft!");
+          winner = 0;
+        } else if (P1 == 1 && P2 == 2
+                   || P1 == 3 && P2 == 1
+                   || P1 == 2 && P2 == 3) {
+            Console.WriteLine("{0} beats {1}! Player 1 wins!", P1, P2);
+            winner = 1;
+        } else if (P1 == 2 && P2 == 1
+                   || P1 == 1 && P2 == 3
+                   || P1 == 3 && P2 == 2) {
+            Console.WriteLine("{0} beats {1}! Player 2 wins!", P2, P1);
+          winner = 2;
+        }
+      return winner;
+    }
+
+
 }
+
 
 
