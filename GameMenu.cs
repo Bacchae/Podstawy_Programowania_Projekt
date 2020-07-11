9782f8b50be3a0a655227bfdd457762089206e94
Bacchae <kciuk.barbara@gmail.com>
2020-07-11 08:40:35 +0000

    new version

--- GameMenu.cs
+++ GameMenu.cs
@@ -23,10 +23,7 @@ class GameMenu {
         adventure.NewAdventure ();
       }
       else if (input == ConsoleKey.D3){
-        GameMenu ();
-      }
-      else {
-        GameMenu ();
+        fight.InitializeFight();
       }
 
   }
